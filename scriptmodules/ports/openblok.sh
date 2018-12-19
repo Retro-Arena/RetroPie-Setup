59bae3a07f78f701c562021f7cbb211a03e3b664
6alileo <42245908+6alileo@users.noreply.github.com>
2018-12-18 16:29:39 -0800

    Update openblok.sh

--- scriptmodules/ports/openblok.sh
+++ scriptmodules/ports/openblok.sh
@@ -12,7 +12,7 @@
 rp_module_id="openblok"
 rp_module_desc="OpenBlok: A Block Dropping Game"
 rp_module_licence="GPL3 https://raw.githubusercontent.com/mmatyas/openblok/master/LICENSE.md"
-rp_module_section="exp"
+rp_module_section="opt"
 rp_module_flags=""
 
 function depends_openblok() {
