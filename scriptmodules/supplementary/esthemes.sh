6ff308a1f47317dfe08616f555c6d6ef6ff539a1
6alileo <6alileo@users.noreply.github.com>
2018-12-28 09:29:02 -0800

    remove gallery dir

--- scriptmodules/supplementary/esthemes.sh
+++ scriptmodules/supplementary/esthemes.sh
@@ -135,15 +135,6 @@ function gui_esthemes() {
         local status=()
         local default
 
-        local gallerydir="/etc/emulationstation/es-theme-gallery"
-        if [[ -d "$gallerydir" ]]; then
-            status+=("i")
-            options+=(G "View or Update Theme Gallery")
-        else
-            status+=("n")
-            options+=(G "Download Theme Gallery")
-        fi
-
         options+=(U "Update all installed themes")
 
         local i=1
@@ -166,33 +157,6 @@ function gui_esthemes() {
         default="$choice"
         [[ -z "$choice" ]] && break
         case "$choice" in
-            G)
-                if [[ "${status[0]}" == "i" ]]; then
-                    options=(1 "View Theme Gallery" 2 "Update Theme Gallery" 3 "Remove Theme Gallery")
-                    cmd=(dialog --backtitle "$__backtitle" --menu "Choose an option for gallery" 12 40 06)
-                    local choice=$("${cmd[@]}" "${options[@]}" 2>&1 >/dev/tty)
-                    case "$choice" in
-                        1)
-                            cd "$gallerydir"
-                            if isPlatform "x11"; then
-                                feh --info "echo %f" --slideshow-delay 6 --fullscreen --auto-zoom --filelist images.list
-                            else
-                                fbi --timeout 6 --once --autozoom --list images.list
-                            fi
-                            ;;
-                        2)
-                            gitPullOrClone "$gallerydir" "https://github.com/Retro-Arena/es-theme-gallery"
-                            ;;
-                        3)
-                            if [[ -d "$gallerydir" ]]; then
-                                rm -rf "$gallerydir"
-                            fi
-                            ;;
-                    esac
-                else
-                    gitPullOrClone "$gallerydir" "http://github.com/Retro-Arena/es-theme-gallery"
-                fi
-                ;;
             U)
                 for theme in "${installed_themes[@]}"; do
                     theme=($theme)
