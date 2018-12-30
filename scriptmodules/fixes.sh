594016a2ba258caff893841ba58a8c6b0ed27dbf
6alileo <6alileo@users.noreply.github.com>
2018-12-29 21:42:22 -0800

    Update fixes.sh

--- scriptmodules/fixes.sh
+++ scriptmodules/fixes.sh
@@ -59,7 +59,7 @@ if [[ ! -f /home/pigaming/scripts/update009 ]]; then
         rm -rf "$home/.emulationstation/gamelists/retropie"
         delSystem retropie
     fi
-    sudo ./RetroPie-Setup/retropie_packages.sh settingsmenu install_bin
-    sudo ./RetroPie-Setup/retropie_packages.sh settingsmenu configure
+    sudo /home/pigaming/RetroPie-Setup/retropie_packages.sh settingsmenu install_bin
+    sudo /home/pigaming//RetroPie-Setup/retropie_packages.sh settingsmenu configure
     touch /home/pigaming/scripts/update009
 fi
